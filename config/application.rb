df7370cb1efa3b13d4f7a320f2a8ac494b7c77d1
Theophile Thiberge <tthiberge@gmail.com>
2023-03-27 21:41:40 +0200

    Extension of tom-select

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module RailsNewWatchList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -33,5 +34,6 @@ module RailsNewWatchList
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
   end
 end
