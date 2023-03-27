2a300fc2ff428403756aa6bf44996c0083fb8fd5
Theophile Thiberge <tthiberge@gmail.com>
2023-03-27 10:00:33 +0200

    Implementing all the proper validations and associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,11 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
+
+  validates :comment, length: {minimum: 6 }
+  validates :movie, uniqueness: {
+    scope: :list,
+    message: 'The movie can join the list only one time'
+  }
+
 end
